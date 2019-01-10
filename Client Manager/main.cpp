ff95b49cd3bbec6ab484f475eff5613b06cd26be
Kirkm9 <Kirkm9@users.noreply.github.com>
2019-01-10 16:53:36 +0100

    Update main.cpp

--- Client Manager/main.cpp
+++ Client Manager/main.cpp
@@ -22,18 +22,27 @@ namespace fs = std::experimental::filesystem;
 
 // sftp port : 14 147
 
-int64_t getSyncDate(rapidjson::Document &d, std::string filename)
+int64_t getSyncDate(rapidjson::Document &fileboxJSON, std::string filename)
 {
-	std::cout << "filename = " << filename << "\n";
-	int numberOfFiles = d.Size();
+	int numberOfFiles = fileboxJSON.Size();
 	for (int i(0); i < numberOfFiles; ++i)
-		if (d[i]["path"].GetString() == filename)
-			return d[i]["syncDate"].GetInt64();
+		if (fileboxJSON[i]["path"].GetString() == filename)
+			return fileboxJSON[i]["syncDate"].GetInt64();
 
 	return -1;
 }
 
-bool sendFile(std::string filename)
+int64_t getLocalDate(rapidjson::Document &localInfoJSON, std::string filename)
+{
+	int numberOfFiles = localInfoJSON.Size();
+	for (int i(0); i < numberOfFiles; ++i)
+		if (localInfoJSON[i]["path"].GetString() == filename)
+			return localInfoJSON[i]["localDate"].GetInt64();
+
+	return -1;
+}
+
+bool sendFile(std::string filename, rapidjson::Document &localInfoJSON, int64_t localDate, std::string shortFilename, std::string localInfo)
 {
 	std::cout << "Need to re-send file (" << filename << ") !" << "\n";
 
@@ -48,12 +57,54 @@ bool sendFile(std::string filename)
 	system(cmd.c_str());
 
 	// Sending file
-	cmd = "curl -T \"" + destination + "\" ftp://client:@127.0.0.1/";
+	cmd = "curl.exe -T \"" + destination + "\" ftp://client:@127.0.0.1/";
 	system(cmd.c_str());
 	
 	// Deleting the copy
 	cmd = "DEL \"" + destination + "\" /F";
 	system(cmd.c_str());
+
+	bool modified = false;
+	int numberOfFiles = localInfoJSON.Size();
+	for (int i(0); i < numberOfFiles; ++i)
+		if (localInfoJSON[i]["path"].GetString() == filename)
+		{
+			localInfoJSON[i]["localDate"].SetInt64(localDate);
+			modified = true;
+		}
+	
+	if (!modified)
+	{
+		std::string objectToAdd = ",{ ";
+		objectToAdd += "\"path\" : \"" + shortFilename + "\",";
+		objectToAdd += "\"localDate\" : " + std::to_string(localDate) + "}";
+
+		{
+			std::ifstream in(localInfo);
+			std::ofstream out(localInfo + "tmp");
+			
+			std::string line;
+			while (getline(in, line)) 
+			{
+				if (line.find("]") == std::string::npos)
+					out << line << '\n';
+			}
+
+			out.close();
+			in.close();
+		}
+
+		cmd = "DEL \"" + localInfo + "\" /F";
+		system(cmd.c_str());
+		std::string cmd = "ECHO F | xcopy \"" + localInfo + "tmp" + "\" \"" + localInfo + "\" ";
+		system(cmd.c_str());
+
+		std::ofstream outfile;
+		outfile.open(localInfo, std::ios_base::app);
+		outfile << objectToAdd;
+		outfile << "\n]";
+		outfile.close();
+	}
 
 	return true;
 }
@@ -64,34 +115,37 @@ bool getFile(std::string filename)
 
 	std::string filenameInFTP = "564853"; // Asking for coping file in temp folder
 
-	std::string cmd = "curl -o \"" + filename + "\" \"ftp://client:@127.0.0.1/" + filenameInFTP + "\"";
+	std::string cmd = "curl.exe -o \"" + filename + "\" \"ftp://client:@127.0.0.1/" + filenameInFTP + "\"";
 	std::cout << cmd << "\n";
 	system(cmd.c_str());
 
 	return true;
 }
 
-void checkSyncDate(rapidjson::Document &d, std::string folderPath, std::string initialFolderPath)
+void checkSyncDate(rapidjson::Document &fileboxJSON, rapidjson::Document &localInfoJSON, std::string folderPath, std::string initialFolderPath, 
+	std::string localFile)
 {
-	for (auto & p : fs::directory_iterator(folderPath))
+	for (auto & p : fs::directory_iterator(folderPath)) // For each files
 	{
 		std::string filename = p.path().string();
-		if (filename.substr(initialFolderPath.size() + 1, SIZE_MAX) == ".filebox")
+		if (filename.substr(initialFolderPath.size() + 1, SIZE_MAX) == ".filebox") // filebox file -> no sync
 			continue;
-		std::cout << filename << std::endl;
-		if (fs::is_directory(p))
+		std::cout << "Filename : " << filename << std::endl;
+		if (fs::is_directory(p)) // it's a directory -> re call function
 		{
-			checkSyncDate(d, filename, initialFolderPath);
+			std::cout << "Entering directory.." << std::endl;
+			checkSyncDate(fileboxJSON, localInfoJSON, filename, initialFolderPath, localFile);
 			continue;
 		}
 		struct stat result;
 		if (stat(filename.c_str(), &result) == 0)
 		{
 			int64_t mod_time = result.st_mtime;
-			int64_t syncDate = getSyncDate(d, filename.substr(initialFolderPath.size() + 1, SIZE_MAX));
+			int64_t syncDate = getSyncDate(fileboxJSON, filename.substr(initialFolderPath.size() + 1, SIZE_MAX));
+			int64_t localDate = getLocalDate(localInfoJSON, filename.substr(initialFolderPath.size() + 1, SIZE_MAX));
 
-			if (mod_time > syncDate)
-				sendFile(filename);
+			if (mod_time > localDate)
+				sendFile(filename, localInfoJSON, localDate, filename.substr(initialFolderPath.size() + 1, SIZE_MAX), localFile);
 			else if (syncDate > mod_time)
 				getFile(filename);
 		}
@@ -119,11 +173,23 @@ int main(int argc, char** argv)
 		}
 		const char* data = text.c_str();
 
-		rapidjson::Document d;
-		d.Parse(data);
+		rapidjson::Document fileboxJSON;
+		fileboxJSON.Parse(data);
 
-		checkSyncDate(d, folderPath, folderPath);
+		line = "", text = "";
+		std::ifstream inLocal(folderPath + "/.localInfo");
+		while (std::getline(inLocal, line))
+		{
+			text += line + "\n";
+		}
+		const char* dataLocal = text.c_str();
+
+		rapidjson::Document localInfoJSON;
+		localInfoJSON.Parse(dataLocal);
+
+		checkSyncDate(fileboxJSON, localInfoJSON, folderPath, folderPath, folderPath + "/.localInfo");
 	}
 
+	std::cout << "End" << "\n";
 	return EXIT_SUCCESS;
 }
