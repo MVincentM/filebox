0cc64b27d7d2fdade001c242c0e4638b4ecd9938
Hippolyte Robichon <hippo.robichon@gmail.com>
2019-03-07 19:37:08 +0100

    commit front

--- public/js/file-explorer.js
+++ public/js/file-explorer.js
@@ -1,50 +1,10 @@
-var root = {
-	type: 'root',
-	children: [
-		{
-			type: 'folder',
-			path: '/',
-			name: 'Nouveau dossier',
-			lastUpdate: '05/12/2017',
-			lastUpdator: 'Jacques',
-			creator: 'Toto'
-		},
-		{
-			type: 'folder',
-			path: '/',
-			name: 'Nouveau dossier 2',
-			lastUpdate: '06/12/2017',
-			lastUpdator: 'Bob',
-			creator: 'Toto',
-		},
-		{
-			type: 'file',
-			path: '/',
-			name: 'notes.txt',
-			lastUpdate: '09/11/2017',
-			version: '1.8',
-			lastUpdator: 'Manu',
-			creator: 'Toto'
-		},
-		{
-			type: 'file',
-			path: '/',
-			name: 'photo.png',
-			lastUpdate: '02/10/2017',
-			version: '1.5',
-			lastUpdator: 'Tom',
-			creator: 'Arthur'
-		},
-		{
-			type: 'file',
-			path: '/',
-			name: 'rapport.pdf',
-			lastUpdate: '30/01/2018',
-			version: '0.2',
-			lastUpdator: 'Gilbert',
-			creator: 'Vincent'
-		}
-	]
+var currentFolder = {
+	id: null,
+	title: "Home"
+}
+
+function refresh(){
+	build(currentFolder.id);
 }
 
 function build(id){
@@ -53,29 +13,83 @@ function build(id){
 		url: "/get/templates/"+id,
 		dataType: "JSON",
 		success: function(data){
-			var mainFolder = data;
+			var fs = data;
 
-			var html = '';
-			for(var i=0; i<mainFolder.length; i++){
-				var f = mainFolder[i];
+			if(fs.length > 0){
+				var html = '';
+				for(var i=0; i<fs.length; i++){
+					var f = fs[i];
 
-				html += '<tr class="'+f.type+'" id="'+f.id+'" path="'+f.path+f.name+'">';
-				html += '<th scope="row">'+ (i+1) +'</th>';
-				html += '<td>'+ f.name +'</td>';
-				html += '<td>'+ f.lastUpdate +'</td>';
-				html += '<td>'+ (f.version ? f.version : '') +'</td>';
-				html += '<td>'+ f.lastUpdator +'</td>';
-				html += '<td>'+ f.creator +'</td>';
-				html += '</tr>';
+					html += '<tr class="'+f.type+'" id="'+f.id+'" title='+f.name+' path="'+f.path+f.name+'">';
+					html += '<th scope="row">'+ (i+1) +'</th>';
+					html += '<td>'+ f.name +'</td>';
+					html += '<td>'+ f.lastUpdate +'</td>';
+					html += '<td>'+ (f.version ? f.version : '') +'</td>';
+					html += '<td>'+ f.lastUpdator +'</td>';
+					html += '<td>'+ f.creator +'</td>';
+					html += '</tr>';
+				}
+
+				jQuery('.empty-table').addClass('hide');
+				jQuery('.file-explorer tbody').html(html);
+			}
+			else{
+				jQuery('.empty-table').removeClass('hide');
+				jQuery('.file-explorer tbody').html('');
 			}
 
-			jQuery('.file-explorer tbody').html(html);
-
 			jQuery('.file-explorer tr.folder').click(function(event){
-				var id = event.currentTarget.getAttribute('id');
-				build(id);
-			})
+				var target = event.currentTarget;
+				currentFolder = {
+				 	id: target.getAttribute('id'),
+				 	title: target.getAttribute('title')
+				}
+				refresh();	
+			});
+
+			jQuery('.add-folder').click(function(){
+				prompt('Add a folder to '+currentFolder.title, 'Enter the name of the new folder:', function(folderName){
+					jQuery.ajax({
+						method: 'POST',
+						url: '/add/folder/'+folderName+'/in/'+currentFolder.id,
+						success: refresh
+					})
+				});
+			});
+
+			jQuery('.add-file').click(function(){
+				
+			});
 		}
 	}); // requete ajax pour recupérer les folders/fichiers
 	
+}
+
+function prompt(title, message, onOk){
+
+	if(jQuery('.prompt-modal')[0] == null){
+		html = '<div class="modal prompt-modal fade" role="dialog"><div class="modal-dialog">';
+		html += '<div class="modal-content"><div class="modal-header">';
+	    html += '<h4 class="modal-title"></h4><button type="button" class="close" data-dismiss="modal">&times;</button></div>';
+	    html += '<div class="modal-body">';
+
+	    // Body
+	    html += '<div class="modal-message"></div>'
+	    html += '<input class="form-control modal-input">';
+
+	    html += '</div><div class="modal-footer"><button type="button" class="btn btn-info btn-ok" data-dismiss="modal">Ok</button><button type="button" class="btn btn-default btn-close" data-dismiss="modal">Close</button></div></div></div></div>';
+
+	    jQuery('body').append(html);
+
+	    jQuery('.prompt-modal .btn-ok').click(function(){
+	    	var value = jQuery('.prompt-modal .modal-input').val();
+	    	onOk(value);
+	    });
+	}
+
+	jQuery('.prompt-modal .modal-title').text(title);
+	jQuery('.prompt-modal .modal-message').text(message);
+	jQuery('.prompt-modal .modal-input').val('');
+
+    jQuery('.prompt-modal').modal('show');
 }
