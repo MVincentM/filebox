d75b6ecad0f388a6ea7a016e5f4a81fc30cd5593
Hippolyte Robichon <hippo.robichon@gmail.com>
2019-03-09 17:59:34 +0100

    commit web front
    fix bug on breadcrumbs
    implement function add files

--- public/js/file-explorer.js
+++ public/js/file-explorer.js
@@ -29,9 +29,10 @@ function build(id){
 				var html = '';
 				for(var i=0; i<fs.length; i++){
 					var f = fs[i];
+					var icon = '/icons/'+f.type+'.png';
 
-					html += '<tr class="'+f.type+'" id="'+f.id+'" title='+f.name+' path="'+f.path+f.name+'">';
-					html += '<th scope="row">'+ (i+1) +'</th>';
+					html += '<tr class="'+f.type+'" id="'+f.id+'" title="'+f.name+'" path="'+f.path+f.name+'">';
+					html += '<th scope="row"><img src="'+icon+'"></th>';
 					html += '<td>'+ f.name +'</td>';
 					html += '<td>'+ f.lastUpdate +'</td>';
 					html += '<td>'+ (f.version ? f.version : '') +'</td>';
@@ -59,17 +60,43 @@ function build(id){
 			});
 
 			$('.add-folder').click(function(){
-				prompt('Add a folder to '+currentFolder.title, 'Enter the name of the new folder:', function(folderName){
+				prompt('Add a folder to '+currentFolder.title, 'Enter the name of the new folder:', function(folderName, close){
 					$.ajax({
 						method: 'POST',
 						url: '/add/folder/'+folderName+'/in/'+currentFolder.id,
-						success: refresh
+						success: function(){
+							close();
+							refresh();
+						}
 					})
 				});
 			});
 
 			$('.add-file').click(function(){
-		
+				askFiles('Add a file to '+currentFolder.title, null, function(files, close){
+					var xhr = new XMLHttpRequest();
+					var progress = document.getElementById('file-upload-progress');
+					$(progress).removeClass('hide');
+
+					xhr.open('POST', '/add/files/in/'+currentFolder.id);
+
+					xhr.upload.addEventListener('progress', function(e) {
+						progress.value = e.loaded;
+						progress.max = e.total;
+					});
+
+					xhr.addEventListener('load', function() {
+						close();
+						refresh();
+					});
+
+					var form = new FormData();
+
+					for(var i=0, c=files.length; i<c; i++)
+						form.append('file', files[0]);
+
+					xhr.send(form);
+				});
 			});
 		}
 	}); // requete ajax pour recupérer les folders/fichiers	
@@ -78,9 +105,10 @@ function build(id){
 function appendToBreardcrumbs(f){
 	if(f.id != 0){
 		$('.breadcrumbs').append('<b>></b><span folder-id="'+f.id+'">'+f.title+'</span>');
-		$('.breadcrumbs span [folder-id="'+f.id+'"]').click(function(event){
+		$('.breadcrumbs span[folder-id="'+f.id+'"]').click(function(event){
 			var id = event.currentTarget.getAttribute('folder-id');
-			build(id);
+			if(id != currentFolder.id)
+				build(id);
 		})
 	}
 }
@@ -107,7 +135,9 @@ function prompt(title, message, onOk){
 
 	    $('.prompt-modal .btn-ok').click(function(){
 	    	var value = $('.prompt-modal .modal-input').val();
-	    	onOk(value);
+	    	onOk(value, function(){
+	    		$('.prompt-modal').modal('hide');
+	    	});
 	    });
 	}
 
@@ -116,4 +146,36 @@ function prompt(title, message, onOk){
 	$('.prompt-modal .modal-input').val('');
 
     $('.prompt-modal').modal('show');
+}
+
+function askFiles(title, message, onOk){
+
+	if($('.askFiles-modal')[0] == null){
+		html = '<div class="modal askFiles-modal fade" role="dialog"><div class="modal-dialog">';
+		html += '<div class="modal-content"><div class="modal-header">';
+	    html += '<h4 class="modal-title"></h4><button type="button" class="close" data-dismiss="modal">&times;</button></div>';
+	    html += '<div class="modal-body">';
+
+	    // Body
+	    html += '<div class="modal-message"></div>'
+	    html += '<input class="modal-input" type="file" multiple />';
+	    html += '<progress id="file-upload-progress" class="hide"></progress>';
+
+	    html += '</div><div class="modal-footer"><button type="button" class="btn btn-info btn-ok">Ok</button><button type="button" class="btn btn-default btn-close" data-dismiss="modal">Close</button></div></div></div></div>';
+
+	    $('body').append(html);
+
+	    $('.askFiles-modal .btn-ok').click(function(){
+	    	var input = $('.askFiles-modal .modal-input')[0];
+	    	onOk(input.files, function(){
+	    		$('.askFiles-modal').modal('hide');
+	    	});
+	    });
+	}
+
+	$('.askFiles-modal .modal-title').text(title);
+	$('.askFiles-modal .modal-message').text(message);
+	$('.askFiles-modal .modal-input').val('');
+
+    $('.askFiles-modal').modal('show');
 }
