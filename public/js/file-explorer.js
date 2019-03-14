33ba50a5c9eb8894d6f3d08ca4cd7b3d8650454b
Hippolyte Robichon <hippo.robichon@gmail.com>
2019-03-14 10:29:38 +0100

    commit web front
    add pop up to manage user access
    autocomplete to search user

--- public/js/file-explorer.js
+++ public/js/file-explorer.js
@@ -101,6 +101,7 @@ function build(id){
 					html += '<td>'+ f.creator +'</td>';
 					html += '<td><img title="Download" class="download" src="/icons/download.png">'
 								+'<img title="Rename" class="edit" src="/icons/edit.png">'
+								+'<img title="Shared" class="user-access" src="/icons/users.png">'
 								+'<img title="Delete" class="delete" src="/icons/delete.png"></td>';
 					html += '</tr>';
 				}
@@ -150,6 +151,16 @@ function build(id){
 					})
 				});
 			});
+
+			$('img.user-access').click(function(event){
+				event.stopPropagation();
+				var target = event.currentTarget.parentElement.parentElement;
+				var name = target.getAttribute('name');
+				var id = target.getAttribute('id');				
+				askUserAccess(target.id, name, function(){
+					console.log('ok');
+				});
+			})
 
 			$('img.delete').click(function(event){
 				event.stopPropagation();
@@ -271,7 +282,7 @@ function alert(type, message){
 	}
 }
 
-function askUserAccess(id, title, message, onOk){
+function askUserAccess(id, name, onOk){
 
 	$('.useraccess-modal').remove();
 
@@ -282,8 +293,8 @@ function askUserAccess(id, title, message, onOk){
 
 	// Body
 	html += '<div class="modal-message"></div>'
-	html += '<input class="form-control modal-input"><button class="btn-add-user">Add user</button>';
-	html += '<div class="user-list"></div>';
+	html += '<div class="autocomplete" style="width:100%;"><input type="text" placeholder="Search a user"></div>';
+	html += '<div class="user-list">No other users has access</div>';
 
 	html += '</div><div class="modal-footer"><button class="btn btn-default btn-close" data-dismiss="modal">Close</button></div></div></div></div>';
 
@@ -297,9 +308,11 @@ function askUserAccess(id, title, message, onOk){
 			});
 	});
 
-	$('.useraccess-modal .modal-title').text(title);
-	$('.useraccess-modal .modal-message').text(message);
+	$('.useraccess-modal .modal-title').text("Give access to others");
+	$('.useraccess-modal .modal-message').text("Enter the user you want to give access to \""+name+"\"");
 	$('.useraccess-modal .modal-input').val('');
+
+	autocomplete($('.useraccess-modal .autocomplete input')[0], ['bibi','momo','dada','riri','bibo','dadon']);
 
 	$.ajax({
 		method: 'POST',
@@ -318,4 +331,150 @@ function askUserAccess(id, title, message, onOk){
 	})
 
     $('.useraccess-modal').modal('show');
+}
+
+function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+
+
+      // JS search
+      /*for each item in the array...*/
+     /* for (i = 0; i < arr.length; i++) {
+        // check if the item starts with the same letters as the text field value:
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          // create a DIV element for each matching element:
+          b = document.createElement("DIV");
+          // make the matching letters bold:
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          // insert a input field that will hold the current array item's value:
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          // execute a function when someone clicks on the item value (DIV element):
+          b.addEventListener("click", function(e) {
+              // insert the value for the autocomplete text field:
+              inp.value = this.getElementsByTagName("input")[0].value;
+              // close the list of autocompleted values, (or any other open lists of autocompleted values:
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }*/
+
+      // Remote search
+      $.ajax({
+      	method: 'POST',
+		url: '/search/'+val,
+		success: function(users){
+			for(var i=0; i<users.length; i++){
+			  // create a DIV element for each matching element:
+	          b = document.createElement("DIV");
+	          // make the matching letters bold:
+	          b.innerHTML = "<strong>" + users[i].substr(0, val.length) + "</strong>";
+	          b.innerHTML += users[i].substr(val.length);
+	          // insert a input field that will hold the current array item's value:
+	          b.innerHTML += "<input type='hidden' value='" + users[i] + "'>";
+	          // execute a function when someone clicks on the item value (DIV element):
+	          b.addEventListener("click", function(e) {
+	              // insert the value for the autocomplete text field:
+	              inp.value = this.getElementsByTagName("input")[0].value;
+	              // close the list of autocompleted values, (or any other open lists of autocompleted values:
+	              closeAllLists();
+
+	              // TODO : appel ajax pour ajouter l'utilisateur
+	          });
+	          a.appendChild(b);
+	        }
+		},
+		error: function(){
+			var users = ['error1','error2','error3','error4'];
+			for(var i=0; i<users.length; i++){
+			  // create a DIV element for each matching element:
+	          b = document.createElement("DIV");
+	          // make the matching letters bold:
+	          b.innerHTML = "<strong>" + users[i].substr(0, val.length) + "</strong>";
+	          b.innerHTML += users[i].substr(val.length);
+	          // insert a input field that will hold the current array item's value:
+	          b.innerHTML += "<input type='hidden' value='" + users[i] + "'>";
+	          // execute a function when someone clicks on the item value (DIV element):
+	          b.addEventListener("click", function(e) {
+	              // insert the value for the autocomplete text field:
+	              inp.value = this.getElementsByTagName("input")[0].value;
+	              // close the list of autocompleted values, (or any other open lists of autocompleted values:
+	              closeAllLists();
+	          });
+	          a.appendChild(b);
+	        }
+		}
+      })
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
 }
